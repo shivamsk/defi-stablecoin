7b73136371fc24807ebfbaa8c8ff2aa558d60c01
Siva Krishna Merugu <msk.iitr@gmail.com>
2024-09-11 12:56:48 +0530

    Modifies mintDsc method logic

--- src/DSCEngine.sol
+++ src/DSCEngine.sol
@@ -36,6 +36,7 @@ contract DSCEngine is ReentrancyGuard {
     error DSCEngine__NotAllowedToken();
     error DSCEngine__TransferFailed();
     error DSCEngine__BreaksHealthFactor(uint256 healthFactor);
+    error DSCEngine__MintFailed();
 
     ////////////////////
     // State Variables
@@ -153,6 +154,10 @@ contract DSCEngine is ReentrancyGuard {
 
         // If they minted too much
         _revertIfHealthFactorIsBroken(msg.sender);
+        bool minted = i_dsc.mint(msg.sender, amountDscToMint);
+        if (!minted) {
+            revert DSCEngine__MintFailed();
+        }
     }
 
     function burnDsc() external {}
