3c016b19bc666ac3ff1f826a34ecea896090ebf7
Siva Krishna Merugu <msk.iitr@gmail.com>
2024-09-17 00:28:12 +0530

    Adds MockFailedTransferFrom

---
+++ test/mocks/MockFailedTransferFrom.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+import {ERC20Burnable, ERC20} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract MockFailedTransferFrom is ERC20Burnable, Ownable {
+    error DecentralisedStableCoin__AmountMustBeGreaterThanZero();
+    error DecentralisedStableCoin__BurnAmountExceedsBalance();
+
+    constructor() ERC20("DecentralisedStableCoin", "DSC") {}
+
+    function burn(uint256 _amount) public override onlyOwner {
+        uint256 balance = balanceOf(msg.sender);
+        if (_amount <= 0) {
+            revert DecentralisedStableCoin__AmountMustBeGreaterThanZero();
+        }
+
+        if (_amount > balance) {
+            revert DecentralisedStableCoin__BurnAmountExceedsBalance();
+        }
+
+        super.burn(_amount);
+    }
+
+    function mint(address account, uint256 amount) public {
+        _mint(account, amount);
+    }
+}
